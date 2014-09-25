49fc8d2dcc77bef7ac6515cfbf31e76bcec233aa
ZhengLiPeng <zhenglipeng@mrocker.com>
2014-09-25 18:29:22 +0800

    开始加入逻辑控制

--- Public/angular/app.js
+++ Public/angular/app.js
@@ -1,3 +1,9 @@
 'use strict';
 
 var app = angular.module('mApp', ['ngRoute','mCtrl','mDire','mFilter']);
+
+app.config(['$routeProvider', function ($routeProvider){
+	$routeProvider
+		.when('/chatbox/:uid', {templateUrl:'/App/Tpl/Chatbox/chatbox.html', controller:'ChatCtrl'})
+		// .otherwise({redirectTo: '/chatbox'});
+}]);
--- Public/angular/controllers/controller.js
+++ Public/angular/controllers/controller.js
@@ -2,11 +2,48 @@
 
 var mCtrl = angular.module('mCtrl',[]);
 
+mCtrl.controller('RootCtrl', function ($rootScope, $http, $routeParams){
+  $rootScope.users = [{
+  	uid : '1',
+  	img : 'http://localhost:82/Uploads/20140924/542299330b7aa.jpg',
+  	nick : '阿拉甲',
+  	latestMsg : '我还有最后一句话...'
+  },{
+  	uid : '2',
+  	img : 'http://localhost:82/Uploads/20140924/542299330b7aa.jpg',
+  	nick : '阿拉乙',
+  	latestMsg : '我也还有最后一句话...'
+  }];
+});
+
 mCtrl.controller('MsgCtrl', function ($scope, $http, $routeParams){
   
-  var appId = $routeParams.appId || '';
   $http.get('/Chatbox/msg').success(function(data){
     console.log(data);
   });
 
+});
+
+mCtrl.controller('MainCtrl', function ($scope, $http, $routeParams){
+  $scope.main = {
+  	tpl : 'user.html',
+  	panel : 'user',
+  	title : '会话'
+  };
+});
+
+mCtrl.controller('UserCtrl', function ($rootScope, $scope, $http, $routeParams){
+  
+});
+
+mCtrl.controller('ChatCtrl', function ($rootScope, $scope, $http, $routeParams){
+
+	var uid = $routeParams.uid || '';
+	for(var i = 0; i < $rootScope.users.length; i++){
+		if(uid == $rootScope.users[i].uid){
+			$scope.user = $rootScope.users[i];
+			break;
+		}
+	}
+  
 });
