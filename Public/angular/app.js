7221fa855e690014a8aaf6adad652fae06b1c7c3
lanyuechen <1252012805@qq.com>
2014-09-27 22:46:42 +0800

    添加好友列表

--- Public/angular/app.js
+++ Public/angular/app.js
@@ -7,5 +7,6 @@ app.config(['$routeProvider', function ($routeProvider){
 		.when('/chatbox/:uid', {templateUrl:'/App/Tpl/Chatbox/chatbox.html', controller:'ChatCtrl'})
 		.when('/search', {templateUrl:'/App/Tpl/Chatbox/search.html', controller:'SearchCtrl'})
 		.when('/detail', {templateUrl:'/App/Tpl/Chatbox/detail.html'})
+		.when('/contact', {templateUrl:'/App/Tpl/Chatbox/contact.html'})
 		// .otherwise({redirectTo: '/chatbox'});
 }]);
