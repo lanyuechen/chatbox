859ae83ca715ccc800080bed0427a84710ee0cce
ZhengLiPeng <zhenglipeng@mrocker.com>
2014-10-14 17:15:59 +0800

    delete php files

--- App/Lib/Model/UserModel.class.php
+++
@@ -1,7 +0,0 @@
-<?php
-
-Class UserModel extends MongoModel {
-	Protected $pk = 'id';
-    Protected $_idType = self::TYPE_INT;
-    protected $_autoInc =  true;
-}
--- index.php
+++
@@ -1,6 +0,0 @@
-<?php
-header('content-type:text/html; charset=utf-8');
-define('APP_DEBUG',TRUE); // 开启调试模式
-define('APP_PATH','./App/');
-require "./ThinkPHP/ThinkPHP.php";
-?>
